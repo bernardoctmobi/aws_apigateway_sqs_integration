d76ee6692ca04f05dbae8965b1a4ea83eca5f52a
Bernardo Di Miceli <bdm.444@gmail.com>
2023-08-22 16:50:06 +0200

    aggiunto codice della lambda

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,22 @@
-export const lambdaHandler = async (event, context) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+export const handler = async (event) => {
+  const records = event.Records;
+  console.log(records);
+  
+  const promisesArray = records.map(record => createPromiseForRecord(record));
+  const results = await Promise.allSettled(promisesArray);
+  console.log(results);
+  const rejectedRecords = results.filter((item) => item.status === "rejected");
+  console.log(rejectedRecords);
+  const batchItemFailures = rejectedRecords.map(item => ({itemIdentifier:item.reason}));
+  console.log(batchItemFailures);
+  return {batchItemFailures};
 };
+
+async function createPromiseForRecord(record) {
+  const parsedBody = JSON.parse(record.body);
+  const result = (typeof(parsedBody.firstNumber) == "number" && typeof(parsedBody.secondNumber) == "number");
+  if (!result) {
+    throw record.messageId;
+  }
+  return (parsedBody.firstNumber + parsedBody.secondNumber);
+}
