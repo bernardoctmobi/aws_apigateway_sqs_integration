ef5be0ca5f8fc4aa8807cd7a52aee188821c8c8f
Bernardo Di Miceli <bdm.444@gmail.com>
2023-08-28 15:46:09 +0200

    eseguito il linting con eslint

--- src/app.mjs
+++ src/app.mjs
@@ -1,23 +1,21 @@
 export const handler = async (event) => {
-  const records = event.Records;
-  
-  const promisesArray = records.map(record => createPromiseForRecord(record));
-  const results = await Promise.allSettled(promisesArray);
+  const records = event.Records
+  const promisesArray = records.map(record => createPromiseForRecord(record))
+  const results = await Promise.allSettled(promisesArray)
   // const rejectedRecords = results.filter((item) => item.status === "rejected");
   // const batchItemFailures = rejectedRecords.map(item => ({ itemIdentifier: item.reason }));
-  
   const batchItemFailures = results.filter(
-    (item) => item.status === "rejected").map(
-    item => ({ itemIdentifier: item.reason }));
-  console.log(batchItemFailures);
-  return {batchItemFailures};
-};
+    (item) => item.status === 'rejected').map(
+    item => ({ itemIdentifier: item.reason }))
+  console.log(batchItemFailures)
+  return { batchItemFailures }
+}
 
-async function createPromiseForRecord(record) {
-  const parsedBody = JSON.parse(record.body);
-  const result = (typeof(parsedBody.firstNumber) == "number" && typeof(parsedBody.secondNumber) == "number");
+async function createPromiseForRecord (record) {
+  const parsedBody = JSON.parse(record.body)
+  const result = (typeof (parsedBody.firstNumber) === 'number' && typeof (parsedBody.secondNumber) === 'number')
   if (!result) {
-    throw record.messageId;
+    throw record.messageId
   }
-  return (parsedBody.firstNumber + parsedBody.secondNumber);
+  return (parsedBody.firstNumber + parsedBody.secondNumber)
 }
