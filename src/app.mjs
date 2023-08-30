4892cab13aba1c280c2cc78615cb71a48d15747f
Bernardo Di Miceli <bdm.444@gmail.com>
2023-08-30 10:23:08 +0200

    corretta la lambda e pulito il suo codice

--- src/app.mjs
+++ src/app.mjs
@@ -4,12 +4,9 @@ export const handler = async (event) => {
   const promisesArray = records.map(record => createPromiseForRecord(record))
   const results = await Promise.allSettled(promisesArray)
   console.log(results)
-  // const rejectedRecords = results.filter((item) => item.status === "rejected");
-  // const batchItemFailures = rejectedRecords.map(item => ({ itemIdentifier: item.reason }));
   const batchItemFailures = results
     .filter(item => item.status === 'rejected')
-    .map(item => ({ itemIdentifier: item.reason }))
-    //  .map(item => ({ itemIdentifier: item.reason.message }))
+    .map(item => ({ itemIdentifier: item.reason.message }))
   console.log(batchItemFailures)
   return { batchItemFailures }
 }
@@ -18,8 +15,7 @@ async function createPromiseForRecord (record) {
   const parsedBody = JSON.parse(record.body)
   const result = (typeof (parsedBody.firstNumber) === 'number' && typeof (parsedBody.secondNumber) === 'number')
   if (!result) {
-    //  throw new Error(record.messageId)
-    throw record.messageId
+    throw new Error(record.messageId)
   }
   return (parsedBody.firstNumber + parsedBody.secondNumber)
 }
