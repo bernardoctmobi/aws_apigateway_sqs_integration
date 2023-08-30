74aa49f51d6ecc1cee47389924f35ed496a95d7d
Bernardo Di Miceli <bdm.444@gmail.com>
2023-08-30 11:24:56 +0200

    cambiata la condizione di rigetto nella lambda e eliminati console.log

--- src/app.mjs
+++ src/app.mjs
@@ -1,9 +1,7 @@
 export const handler = async (event) => {
   const records = event.Records
-  console.log(records)
   const promisesArray = records.map(record => createPromiseForRecord(record))
   const results = await Promise.allSettled(promisesArray)
-  console.log(results)
   const batchItemFailures = results
     .filter(item => item.status === 'rejected')
     .map(item => ({ itemIdentifier: item.reason.message }))
@@ -13,7 +11,7 @@ export const handler = async (event) => {
 
 async function createPromiseForRecord (record) {
   const parsedBody = JSON.parse(record.body)
-  const result = (typeof (parsedBody.firstNumber) === 'number' && typeof (parsedBody.secondNumber) === 'number')
+  const result = (Number.isInteger(parsedBody.firstNumber) && (Number.isInteger(parsedBody.secondNumber)))
   if (!result) {
     throw new Error(record.messageId)
   }
